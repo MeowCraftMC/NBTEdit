037f475191ed64e7e5c4dfa62bccb0910fbbc4a6
秋雨落 <i@rain.cx>
2025-01-26 10:53:36 +0800

    :bug: Fix #50, #51. Use vanilla impl as fallback.
    There is no method to determine have a permission provider or not.
    But just an api anyone can shade.
    
    Signed-off-by: 秋雨落 <i@rain.cx>

--- nbtedit-fabric/src/main/java/cx/rain/mc/nbtedit/fabric/command/FabricPermissionApiImpl.java
+++ nbtedit-fabric/src/main/java/cx/rain/mc/nbtedit/fabric/command/FabricPermissionApiImpl.java
@@ -2,20 +2,26 @@ package cx.rain.mc.nbtedit.fabric.command;
 
 import cx.rain.mc.nbtedit.api.command.IModPermission;
 import cx.rain.mc.nbtedit.api.command.ModPermissions;
+import cx.rain.mc.nbtedit.fabric.config.ModConfigImpl;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
 
 public class FabricPermissionApiImpl implements IModPermission {
+    private final ModConfigImpl config;
+
+    public FabricPermissionApiImpl(ModConfigImpl config) {
+        this.config = config;
+    }
 
     @Override
     public boolean hasPermission(@NotNull CommandSourceStack sourceStack, @NotNull ModPermissions permission) {
-        return Permissions.check(sourceStack, "nbtedit.%s".formatted(permission.getName()));
+        return Permissions.check(sourceStack, "nbtedit.%s".formatted(permission.getName()), config.getPermissionsLevel(permission));
     }
 
     @Override
     public boolean hasPermission(@NotNull ServerPlayer player, @NotNull ModPermissions permission) {
-        return Permissions.check(player, "nbtedit.%s".formatted(permission.getName()));
+        return Permissions.check(player, "nbtedit.%s".formatted(permission.getName()), config.getPermissionsLevel(permission));
     }
 }
